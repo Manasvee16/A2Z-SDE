4396d980f1be492cfce8900e86cd625181f0525b
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-07-14 01:35:46 +0530

    Update Rearrange Array Elements by Sign.cpp

--- Rearrange Array Elements by Sign.cpp
+++ Rearrange Array Elements by Sign.cpp
@@ -21,3 +21,59 @@ public:
         return ans;
     }
 };
+
+//recursion
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> ans(nums.size(), 0);
+        helper(nums, 0, ans, 0, 1);
+        return ans;
+    }
+    void helper(vector<int>& nums, int index, vector<int>& ans, int even, int odd) {
+        if (index == nums.size())
+            return;
+        if (nums[index] > 0) {
+            ans[even] = nums[index];
+            helper(nums, index + 1, ans, even + 2, odd);
+        } 
+        else {
+            ans[odd] = nums[index];
+            helper(nums, index + 1, ans, even, odd + 2);
+        }
+    }
+};
+//TC O(N)
+//SC O(N)
+
+//DP
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 0);
+        vector<bool> dp(n, false); // marks if ans[i] is filled
+        helper(nums, 0, ans, 0, 1, dp);
+        return ans;
+    }
+    void helper(vector<int>& nums, int index, vector<int>& ans, int even, int odd, vector<bool>& dp) {
+        if (index == nums.size())
+            return;
+        if (nums[index] > 0) {
+            if (!dp[even]) {
+                ans[even] = nums[index];
+                dp[even] = true;
+            }
+            helper(nums, index + 1, ans, even + 2, odd, dp);
+        } 
+        else {
+            if (!dp[odd]) {
+                ans[odd] = nums[index];
+                dp[odd] = true;
+            }
+            helper(nums, index + 1, ans, even, odd + 2, dp);
+        }
+    }
+};
+//TC O(N)
+//SC O(N)
