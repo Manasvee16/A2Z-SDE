ded7f81c92bba789f8fd1a69cc25a1c980fee0a8
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-07-14 01:40:44 +0530

    Update Best Time to Buy and Sell Stock.cpp

--- Best Time to Buy and Sell Stock.cpp
+++ Best Time to Buy and Sell Stock.cpp
@@ -21,3 +21,41 @@ public:
 };
 //TC O(N)
 //SC O(1)
+
+//recursion
+class Solution {
+public:
+    int helper(vector<int>& prices, int i, int minSoFar) {
+        if (i == prices.size()) 
+            return 0;
+        int profitToday = prices[i] - minSoFar;
+        int newMin = min(minSoFar, prices[i]);
+        return max(profitToday, helper(prices, i + 1, newMin));
+    }
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty()) 
+            return 0;
+        return helper(prices, 1, prices[0]);
+    }
+};
+
+//DP
+class Solution {
+public:
+    int helper(vector<int>& prices, int i, int minSoFar, vector<int>& dp) {
+        if (i == prices.size()) 
+            return 0;
+        if (dp[i] != -1) 
+            return dp[i];
+        int profitToday = prices[i] - minSoFar;
+        int newMin = min(minSoFar, prices[i]);
+        return dp[i] = max(profitToday, helper(prices, i + 1, newMin, dp));
+    }
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty()) 
+            return 0;
+        int n = prices.size();
+        vector<int> dp(n, -1); //dp[i] stores the maximum profit achievable starting from day i onward, assuming we already know the minimum stock price encountered so far
+        return helper(prices, 1, prices[0], dp);
+    }
+};
