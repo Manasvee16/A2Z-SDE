65554511693ab1f1ad9fc08f41e641d8381961c9
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-07-25 14:45:49 +0530

    Update Sort Colors.cpp

--- Sort Colors.cpp
+++ Sort Colors.cpp
@@ -1,3 +1,28 @@
+class Solution 
+{
+public:
+    void sortColors(vector<int>& nums) 
+    {
+        int n=nums.size();
+        int cnt0=0,cnt1=0,cnt2=0,i;
+        for(int i=0;i<n;i++)
+        {
+            if (nums[i]==0)
+                cnt0++;
+            else if (nums[i]==1)
+                    cnt1++;
+            else
+                cnt2++;
+        }
+        for (i=0;i<cnt0;i++)
+            nums[i]=0;
+        for (i=cnt0;i<cnt0+cnt1;i++)
+            nums[i]=1;
+        for (i=cnt0+cnt1;i<cnt0+cnt1+cnt2;i++)
+            nums[i]=2;
+    }
+};
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
