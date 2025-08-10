1c5c3f4d24c176631589680e41532e865fcaa080
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-08-11 00:08:48 +0530

    Update Find Duplicate.cpp

--- Find Duplicate.cpp
+++ Find Duplicate.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        // Phase 1: find meeting point inside the cycle
+        do {
+            slow = nums[slow];         // one step
+            fast = nums[nums[fast]];   // two steps
+        } while (slow != fast);
+        // Phase 2: find cycle entry (duplicate)
+        fast = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow; // duplicate value
+    }
+};
+//TC O(2N)
+//SC O(N)
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
         int fast=nums[0], slow=nums[0], n=nums.size();
         do
         {
