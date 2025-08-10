7d165086e768fc6ff067e33c8e4bca75b775dc2c
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-08-10 20:33:24 +0530

    Update Array Leaders.cpp

--- Array Leaders.cpp
+++ Array Leaders.cpp
@@ -1,4 +1,24 @@
 class Solution {
+public:
+    vector<int> leaders(vector<int>& arr) {
+        vector<int> result;
+        int n = arr.size();
+        int maxFromRight = INT_MIN;
+        // Traverse from right to left
+        for (int i = n - 1; i >= 0; --i) {
+            if (arr[i] >= maxFromRight) {
+                result.push_back(arr[i]);
+                maxFromRight = arr[i];
+            }
+        }
+        // The leaders are collected in reverse order, so reverse the result
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
+
+
+class Solution {
   public:
     vector<int> leaders(vector<int>& arr) {
         stack<int> st;
