ff977db14e0d035303b77c73f9b9991a87191a52
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-08-11 01:23:19 +0530

    Update Unique Paths.cpp

--- Unique Paths.cpp
+++ Unique Paths.cpp
@@ -1,3 +1,20 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int N=n+m-2; //total number of moves you need to make.
+        //If you start at (0,0) and end at (m-1, n-1), you move (m - 1) times down, (n - 1) times right
+        int r=m-1; //no. of down moves
+        double res=1;
+        for (int i=1;i<=r;i++)
+        {
+            res=res*(N-r+i)/i; //Out of N steps, choose which r=m−1 of them will be the down moves.
+        }
+        return (int) res;
+    }
+};
+//TC O(M)
+//SC O(1)
+
 class Solution 
 {
 public:
