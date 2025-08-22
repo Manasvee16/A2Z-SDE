d3922662ce8da252c78af25f45f6a538981d415a
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-08-22 22:27:37 +0530

    Update String to Integer (atoi).cpp

--- String to Integer (atoi).cpp
+++ String to Integer (atoi).cpp
@@ -30,3 +30,43 @@ public:
         return (int)(num * sign);
     }
 };
+
+class Solution {
+public:
+    // Recursive function to skip whitespaces
+    int skipSpaces(string &s, int i) {
+        if (i < s.size() && s[i] == ' ')
+            return skipSpaces(s, i + 1); // keep skipping
+        return i; // return first non-space index
+    }
+    // Recursive function to convert digits into number
+    long long buildNumber(string &s, int i, int sign, long long num) {
+        // base case: if out of bounds or not a digit -> stop
+        if (i >= s.size() || !isdigit(s[i]))
+            return num * sign;
+        // step: add digit
+        num = num * 10 + (s[i] - '0');
+        // overflow handling
+        if (sign == 1 && num > INT_MAX) 
+            return INT_MAX;
+        if (sign == -1 && -num < INT_MIN) 
+            return INT_MIN;
+        // recursive call with next character
+        return buildNumber(s, i + 1, sign, num);
+    }
+    int myAtoi(string s) {
+        int i = 0;
+        int n = s.size();
+        // Step 1: skip spaces using recursion
+        i = skipSpaces(s, i);
+        // Step 2: check sign
+        int sign = 1;
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            sign = (s[i] == '-') ? -1 : 1;
+            i++;
+        }
+        // Step 3: build number using recursion
+        long long num = buildNumber(s, i, sign, 0);
+        return (int)num;
+    }
+};
